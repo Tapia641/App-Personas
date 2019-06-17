33c8ab813d1503bf0d1a44d1cacc45ffc011966a
Luis Enrique Tapia <tapia641@gmail.com>
2019-06-16 22:04:19 -0500

    Formulario agregado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PersonaComponent } from './persona/persona.component';
-import { PersonasComponent } from './personas/personas.component';
 
 
 // FUNCIONA COMO EVENT
@@ -12,8 +10,6 @@ import { FormsModule } from "@angular/forms";
 @NgModule({
   declarations: [
     AppComponent,
-    PersonaComponent,
-    PersonasComponent
   ],
   imports: [
     BrowserModule,
